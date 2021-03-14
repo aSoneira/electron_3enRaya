219caa08010a42460f2f2bb79624aa1aaeff082a
Alberto Soneira <asalgado.cv@gmail.com>
2021-03-14 11:43:38 +0100

    Supresión de 'Compilados'

--- compilados/constantes.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ANCHO = exports.ALTO = void 0;
-exports.ALTO = 3;
-exports.ANCHO = 3;
-//# sourceMappingURL=constantes.js.map
